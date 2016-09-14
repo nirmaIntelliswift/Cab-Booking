5ebf34664c437298104e4d6014487979b837947d
Nirma <nirma.garg@intelliswift.co.in>
2016-09-14 10:02:32 +0530

    Initial Commit

---
+++ CabBooking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CabBooking
+//
+//  Created by Nirma on 14/09/16.
+//  Copyright © 2016 Nirma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CabBooking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CabBooking
+//
+//  Created by Nirma on 14/09/16.
+//  Copyright © 2016 Nirma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
